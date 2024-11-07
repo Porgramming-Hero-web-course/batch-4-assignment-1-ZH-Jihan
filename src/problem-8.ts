b54d0713b71cfaf7743e598a00efe2326d32d7c3
Zakir Hossain <zakirhossain19920@gmail.com>
2024-11-07 23:50:56 +0600

    solve problem-8

---
+++ src/problem-8.ts
@@ -0,0 +1,14 @@
+
+type Person = {
+    name: string;
+    age: number;
+    email: string;
+    address?:string;
+}
+function validateKeys<T extends object > (obj : T, keys: (keyof T)[]): boolean {
+    const listOfobjectKeys = Object.keys(obj) as (keyof T)[];
+    return keys.every(key => listOfobjectKeys.indexOf(key) !== -1); // help from google and chatGpt only for this line.
+}
+
+const persoN : Person = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(validateKeys(persoN, ["name", "address"]));
