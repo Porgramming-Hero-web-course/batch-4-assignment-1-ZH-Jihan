804562944507964714d2fdfa9211ae043ca1b6fb
Zakir Hossain <zakirhossain19920@gmail.com>
2024-11-07 20:53:00 +0600

    solve problem-4

---
+++ src/problem-4.ts
@@ -0,0 +1,34 @@
+
+
+type Circle = {
+    shape: string,
+    radius:number
+}
+type Rectangle = {
+    shape:string,
+    width: number,
+    height: number,
+}
+
+type Shape = Circle | Rectangle;
+
+function calculateShapeArea ( object : Shape) : number{
+    let result : number = 0
+    if(object.shape.toLowerCase() === "circle" ){
+        const data = (Math.PI * (object as Circle).radius* (object as Circle).radius).toFixed(2);
+        result = parseFloat(data)
+    }else if(object.shape.toLowerCase() === "rectangle" ){
+        result = (object as Rectangle).height * (object as Rectangle).width;
+    }
+    return result;
+}
+
+const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
+const rectangleArea = calculateShapeArea({
+    shape: "rectangle",
+    width: 4,
+    height: 6,
+  });
+
+  console.log(circleArea,rectangleArea);
+  
